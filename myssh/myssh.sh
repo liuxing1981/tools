fac62f28b47ae08dd7675e8d98a2a3344a02acd1
luis.liu <xing.1.liu@nokia-sbell.com>
2019-04-20 00:35:20 +0800

    fix bug myssh pubkey

--- myssh/myssh.sh
+++ myssh/myssh.sh
@@ -5,7 +5,8 @@
 # Created Time: 2018-12-15 09:57
 #########################################################################
 #!/bin/bash
-#args: pgw1
+#args: cbam118
+PUB_KEY_HOME=/home/xliu074/github/tools/myssh/
 if [ -f ~/ansible.host ];then
 	CONFIG=~/ansible.host
 else 
@@ -24,11 +25,13 @@ USER=`echo $line | egrep -o "ansible_ssh_user=\w+" | awk -F= '{print $2}'`
 SERVER=`echo $line | egrep -o "ansible_ssh_host=\w+\.\w+\.\w+\.\w+" | awk -F= '{print $2}'`
 PASSWORD=`echo $line | egrep -o "ansible_ssh_pass=\w+" | awk -F= '{print $2}'`
 PORT=`echo $line | egrep -o "ansible_ssh_ports\w+" | awk -F= '{print $2}'`
-${PORT:=22}
-
+if [ -z "$PORT" ];then
+	PORT=22
+fi
+PUB_KEY=${PUB_KEY_HOME}/${SERVER}.pem
 login_remote(){
-	if [ -f "$SERVER.pem" ];then
-		cmd="ssh -p $PORT -o stricthostkeychecking=no -i $SERVER.pem $USER@$SERVERi $command"
+	if [ -f "$PUB_KEY" ];then
+		cmd="ssh -p $PORT -o stricthostkeychecking=no -i $PUB_KEY $USER@$SERVER $command"
 	else
 		cmd="sshpass -p $PASSWORD ssh -p $PORT -o stricthostkeychecking=no -tt $USER@$SERVER $command"
 	fi
