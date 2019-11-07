edf5aa9e479216ed78324c52bb52cd797676f5d2
xliu074 <xing.1.liu@nokia-sbell.com>
2019-11-07 15:16:13 +0800

    vim bundle

---
+++ vim/install.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: install.sh
+# Author: xliu074
+# mail: xing.1.liu@nokia-sbell.com
+# Created Time: 2019-11-07 15:12
+#########################################################################
+#!/bin/bash
+cp vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
