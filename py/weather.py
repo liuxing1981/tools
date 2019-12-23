f4e1e3d365140cd8fd1a87cb21dd1da142604eb6
luis <lx_figo@qq.com>
2019-12-23 23:29:50 +0800

    add weather

---
+++ py/weather.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import json
+from pprint import pprint
+import requests
+import sys
+
+
+def parse(obj):
+    low = obj['low'].replace('低温 ', '')
+    high = obj['high'].replace('高温 ', '')
+    temp = low + '~' + high
+    type = obj['type']
+    return type + ' ' + temp
+
+
+url = 'http://wthrcdn.etouch.cn/weather_mini?city=青岛'
+result = requests.get(url).content.decode('utf-8')
+result = json.loads(result)
+size = len(sys.argv)
+# no param
+if size == 1:
+    day = result['data']['forecast'][0]
+# one param
+elif size > 1:
+    index = int(sys.argv[1])
+    if index > 0:
+        day = result['data']['forecast'][index]
+    elif index == -1:
+        day = result['data']['yesterday']
+weather = parse(day)
+print(weather)
