37b585803912de794c04f9c4e1a3e936e8b18d5b
xliu074 <xing.1.liu@nokia-sbell.com>
2020-01-07 14:15:47 +0800

    move mailUtils to other project

--- py/mailUtils/MailServer.py
+++
@@ -1,8 +0,0 @@
-class QQServer:
-    POP = 'pop.qq.com:995'
-    IMAP = 'imap.qq.com:993'
-    SMTP = 'smtp.qq.com:465'
-
-class NokiaServer:
-    IMAP = 'mail.int.nokia-sbell.com:993'
-    SMTP = 'mail.int.nokia-sbell.com:25'
--- py/mailUtils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .MailServer import *
-from .Criteria import *
-from .Mail import *
-from .ReceiveMail import *
-from .SendMail import *
